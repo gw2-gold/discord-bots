1102b51ffadd283b1893c1bf3d49fb23d53ac18e
jeffshaver <jeff.e.shaver@gmail.com>
2018-03-03 18:00:35 -0500

    have the bot DM new members with a message to change their nickname

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const discord = require('discord.js')
 
 // utilities
 const cleanupEvents = require('./cleanup-events')
+const { tab } = require('./constants')
 const parseMessage = require('./parse-message')
 
 // message handlers
@@ -33,6 +34,10 @@ bot.on('ready', () => {
 })
 
 bot.on('message', message => {
+  if (!message.content || message.content === '') {
+    return
+  }
+
   const [command, args] = parseMessage(message.content)
   let response
   let shouldPM = false
@@ -88,15 +93,34 @@ bot.on('message', message => {
 })
 
 bot.on('guildMemberAdd', member => {
+  const isBot = member.user.bot
   const channel = member.guild.channels.find('name', 'general')
 
-  if (!channel) return
+  if (!channel || isBot) return
 
   channel.send(
-    `${member.guild.roles.find('name', '@everyone')} please welcome **${
+    `${member.guild.roles.find('name', '@everyone')} please welcome ${
       member.user
-    }** to the family!`
+    } to the family! ${
+      member.user
+    } please check your DMs for a message from me!`
   )
+
+  member.send([
+    `Hey **${
+      member.user.username
+    }**! We are really excited to have you in the [GOLD] family.`,
+    `In order to make it easier for everyone to get to know you, can you please follow the steps below to change your 'nickname' on the [GOLD] server:`,
+    `${tab}1) Click on the [GOLD] server from your list of severs on the left side of discord`,
+    `${tab}2) Click on the down arrow, next to where it says "Wealth of Heroes [GOLD] above the channels list`,
+    `${tab}3) Click "Change Nickname"`,
+    `${tab}You can change this to anything you want, as long as it includes your in-game account name.`,
+    `${tab}For instance, if your account name is **Leo.1234**, you could choose something like:`,
+    `${tab}${tab}- Leo.1234`,
+    `${tab}${tab}- Leo`,
+    `${tab}${tab}- A Name you Choose/Leo`,
+    `${tab}${tab}- A Name you Choose (Leo)`
+  ])
 })
 
 bot.login(TOKEN)
