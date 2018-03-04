90b46274fb60a794aa277ca78539cb13f6e02d0c
jeffshaver <jeff.e.shaver@gmail.com>
2018-03-03 21:03:53 -0500

    fix an issue with the !missions command

--- message-handlers/missions.js
+++ message-handlers/missions.js
@@ -9,6 +9,21 @@ const missions = message => {
     .seconds(0)
     .day('Sunday')
 
+  if (now.isAfter(sunday)) {
+    if (now.diff(sunday, 'hours') < 2) {
+      return [
+        `${
+          message.author
+        }, Guild Missions are every Saturday Evening [US/Can/Alaska], Sunday Morning [OcX/SEA] at 1:00AM server time.`,
+        `Guild missions started **${now.to(
+          sunday
+        )}**. You may be able to catch part of it if you hurry!`
+      ]
+    }
+
+    sunday.day(7)
+  }
+
   return [
     `${
       message.author
