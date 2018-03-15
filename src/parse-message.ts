ac58437f07dac139c08a5d23bd7a65e29a9629b1
jeffshaver <jeff.e.shaver@gmail.com>
2018-03-15 08:13:18 -0400

    log message if we couldn't parse message

--- src/parse-message.ts
+++ src/parse-message.ts
@@ -5,12 +5,19 @@ const mentionsRegex = /<@.+?>/
 
 const parseMessage = (message: string): [string, string[]] => {
   const matches: string[] = <string[]>message.match(regex)
+
+  if (!matches) {
+    console.warn(`could not parse the message: ${message}`)
+
+    return ['', ['']]
+  }
+
   const [command, ...rest] = matches
   // Filter out the mentions. Mentions always start
   // with `<@` and end with `>`. This is
   const args = rest.filter(arg => !mentionsRegex.test(arg))
 
-  const ret: [string, string[]] = [command, args]
+  const ret: [string, string[]] = [command.toLowerCase(), args]
 
   return ret
 }
