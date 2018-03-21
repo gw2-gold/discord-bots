529d4e8f8846119b908f10f4a5fa88369d4c3df2
jeffshaver <jeff.e.shaver@gmail.com>
2018-03-21 15:16:52 -0400

    fix help description for raid command

--- src/commands/raid/index.ts
+++ src/commands/raid/index.ts
@@ -5,12 +5,16 @@ import moment from 'moment'
 import getGuild from '../../get-guild'
 
 const description =
-  "I'll come to your aid and send you this message again, with any added/updated commands!"
+  "I'll send you general time info about our raids as well as how long it is until our next raid"
 const shouldDM = false
 
 const fn = (): Embed => {
   const guild = getGuild()
   const raidSignupChannel = guild.channels.find('name', 'raid-signup')
+  const description = [
+    'Raids are every Saturday Evening [US/Can/Alaska], Sunday Morning [OcX/SEA] at 1:00AM server time.',
+    `To signup for Raids, go to ${raidSignupChannel}`
+  ].join('\n\n')
   const now = moment().utc()
   const sunday = moment()
     .utc()
@@ -23,10 +27,7 @@ const fn = (): Embed => {
     if (now.diff(sunday, 'hours') < 2) {
       return {
         title: `The Raid session started **${now.to(sunday)}**`,
-        description: [
-          'Raids are every Saturday Evening [US/Can/Alaska], Sunday Morning [OcX/SEA] at 1:00AM server time.',
-          `To signup for Raids, go to ${raidSignupChannel}`
-        ].join('\n\n'),
+        description,
         footer: { text: 'You may be able to catch part of it if you hurry!' }
       }
     }
@@ -36,10 +37,7 @@ const fn = (): Embed => {
 
   return {
     title: `The next Raid session is ${now.to(sunday)}`,
-    description: [
-      'Raids are every Saturday Evening [US/Can/Alaska], Sunday Morning [OcX/SEA] at 1:00AM server time.',
-      `To signup for Raids, go to ${raidSignupChannel}`
-    ].join('\n\n'),
+    description,
     footer: {
       text: 'Hopefully we will see you there!'
     }
