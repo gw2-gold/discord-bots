074c861d7ac0284da20fd2871eac3818db01625f
jeffshaver <jeff.e.shaver@gmail.com>
2018-04-24 22:05:21 -0400

    make officer role names an option in .env

--- src/utilities/is-officer.ts
+++ src/utilities/is-officer.ts
@@ -1,11 +1,17 @@
 // Types
 import { GuildMember, Role } from 'discord.js'
 
-const officerRoles = ['Grand Master', 'High Councillor', 'Kings Guard']
+if (!process.env.OFFICER_ROLES) {
+  throw new Error(
+    'OFFICER_ROLES is undefined in the .env file. This is required'
+  )
+}
+
+const OFFICER_ROLES: string[] = process.env.OFFICER_ROLES.split(', ')
 
 const isOfficer = (guildMember: GuildMember) => {
   const role: Role = guildMember.roles.find(role =>
-    officerRoles.includes(role.name)
+    OFFICER_ROLES.includes(role.name)
   )
 
   return role != undefined
